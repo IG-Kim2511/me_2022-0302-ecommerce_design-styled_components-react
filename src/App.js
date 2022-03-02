13f93cbbdd4313a3ae6cfe6a391c26d63ea68aca
IG <iikim2511@gmail.com>
2022-03-02 13:33:55 -0500

    login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 
 import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
 
 
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
-      <Home/> 
-
+      {/* <Home/> 
+      ProductList
+      Register
+      Cart
+      */}
+      
+      <Login/>
     </div>
   );
 }
