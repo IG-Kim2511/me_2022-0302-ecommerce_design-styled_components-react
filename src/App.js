71d8c5cba5a6a032b545f36b21b1508778f43d99
IG <iikim2511@gmail.com>
2022-03-01 23:16:55 -0500

    font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
+import Home from './pages/Home.jsx'
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    hello
+      <Home/> 
 
     </div>
   );
---
+++ src/components/Announcement.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styledComponents from 'styled-components'
+
+
+const Container = styledComponents.div`
+
+height: 30px;
+
+
+ font-size: 14px;
+`;
+
+
+const Announcement = () => {
+  return (
+    <Container>
+
+    Super Deal! free shipping
+    
+    
+    
+    </Container>
+
+  )
+}
+
+export default Announcement
