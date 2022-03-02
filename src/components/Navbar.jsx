680dc3e570bfee576fcb303722003e4d8fb3ce50
IG <iikim2511@gmail.com>
2022-03-02 00:28:00 -0500

    navbar-1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,55 @@
+
 import React from 'react'
+import styledComponents from 'styled-components'
 
-import { MdBadge } from "react-icons/md";
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
 
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+const Container = styledComponents.div``;
+const Wraper = styledComponents.div``;
+const Left = styledComponents.div``;
+const Language = styledComponents.div``;
+const SearchContainer = styledComponents.div``;
+const Search = styledComponents.div``;
+const Center = styledComponents.div``;
+const Logo = styledComponents.div``;
+const Right = styledComponents.div``;
+const MenuItem = styledComponents.div``;
 
 
 const Navbar = () => {
   return (
-    <div>Navbar
+    <Container>
+      <Wraper>
+        <Left>
+          <Language>
+            <SearchContainer>
+              <input placeholder='Search'/>
+              <Search></Search>
+            
+            </SearchContainer>
+          </Language>
+        </Left>
 
-    <MdBadge/>
+        <Center>
+          <Logo>IG</Logo>
+        </Center>
+      
+        
+        <Right>
+          <MenuItem></MenuItem>
+          <MenuItem></MenuItem>
+          <MenuItem>
 
-    
-    </div>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartIcon color="action" />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wraper>    
+    </Container>
   )
 }
 
