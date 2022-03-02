15501789dec35a87fcd164fb5491c0d4e2e26644
IG <iikim2511@gmail.com>
2022-03-01 23:58:48 -0500

    react-icons

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { MdBadge } from "react-icons/md";
+
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar
+
+    <MdBadge/>
+
+    
+    </div>
+  )
+}
+
+export default Navbar
